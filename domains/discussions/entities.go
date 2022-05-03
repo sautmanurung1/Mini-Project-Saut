29cd68ef0248f953de2f97fa6c43ac20cabf334a
sautmanurung1 <smanurung360@gmail.com>
2022-05-03 12:00:30 +0700

    Update entities file discussions

--- domains/discussions/entities.go
+++ domains/discussions/entities.go
@@ -9,13 +9,13 @@ import (
 
 type Discussions struct {
 	gorm.Model
-	UserId       int               `gorm:"not null"`
+	UserId       int               `gorm:"not null" json:"user_id"`
 	User         auth.User         `gorm:"ForeignKey:UserId;References:ID;null" json:"-"`
 	Name         string            `json:"name" validate:"required"`
-	QuestionId   int               `gorm:"not null"`
+	QuestionId   int               `gorm:"not null" json:"question_id"`
 	Question     question.Question `gorm:"ForeignKey:QuestionId;References:ID;null" json:"-"`
 	QuestionUser string            `json:"question_user" validate:"required"`
-	AnswareId    int               `gorm:"not null"`
+	AnswareId    int               `gorm:"not null" json:"answare_id"`
 	Answare      answare.Answare   `gorm:"ForeignKey:AnswareId;References:ID;null" json:"-"`
 	AnswereUser  string            `json:"answare_user" validate:"required"`
 }
