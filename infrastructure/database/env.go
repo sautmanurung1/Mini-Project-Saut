73e3c1a0747c430c32b4e0580c9243c2797653a9
sautmanurung1 <smanurung360@gmail.com>
2022-05-03 10:46:55 +0700

    Update Environment file

--- infrastructure/database/env.go
+++ infrastructure/database/env.go
@@ -1,15 +1,10 @@
 package database
 
 import (
-	"github.com/joho/godotenv"
 	"os"
 )
 
 func ENVDatabase() Config {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
 	return Config{
 		DB_USERNAME: os.Getenv("DB_USERNAME"),
 		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
