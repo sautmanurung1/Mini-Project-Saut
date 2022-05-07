a8f8b6be26a43db5c6292136f5e72539cd6b288d
sautmanurung1 <smanurung360@gmail.com>
2022-05-07 23:57:59 +0700

    Finish All Task

--- entities/question_entities.go
+++ entities/question_entities.go
@@ -9,11 +9,10 @@ type Question struct {
 	AssignmentId    int        `gorm:"not null" json:"assignment_id"`
 	AssignmentTitle string     `gorm:"not null" json:"assignment_title"`
 	AssignmentsId   Assignment `gorm:"ForeignKey:AssignmentId;References:ID;null" json:"-"`
-	// AssignmentsTitle Assignment `gorm:"ForeignKey:AssignmentTitle;References:Title;null" json:"-"`
-	UserId       int    `gorm:"not null" json:"user_id"`
-	User         User   `gorm:"ForeignKey:UserId;References:ID" json:"-"`
-	QuestionUser string `json:"question_user" validate:"required"`
-	Name         string `json:"name" validate:"required" gorm:"column:name"`
+	UserId          int        `gorm:"not null" json:"user_id"`
+	User            User       `gorm:"ForeignKey:UserId;References:ID" json:"-"`
+	QuestionUser    string     `json:"question_user" validate:"required"`
+	Name            string     `json:"name" validate:"required" gorm:"column:name"`
 }
 
 func (*Question) TableName() string {
